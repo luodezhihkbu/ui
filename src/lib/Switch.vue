56f12d1d8751c3272ce66186545fda86874cfc43
Davis <326461396@qq.com>
2021-09-20 21:52:20 +0800

    优化 Switch.vue 的样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -26,29 +26,41 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: gray;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-button.checked {
-  background: blue;
-}
-span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2/2;
-  transition: left 250ms; // 表示如果 left 变化了，变化的时间为 250ms
-}
-button.checked > span {
-  // 可以理解为，先把 left 设为 button 的宽度，再减去 $h2 和 2px；
-  // $h2 是变量，所以要使用 #{}
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus {
-  outline: none;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2/2;
+    transition: all 250ms; // 表示如果 span 任意一个属性的值的发生变化了，变化的时间为 250ms
+  }
+  &.checked {
+    background: #1890ff;
+    > span {
+      // 可以理解为，先把 left 设为 button 的宽度，再减去 $h2 和 2px；
+      // $h2 是变量，所以要使用 #{}
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:focus {
+    outline: none;
+  }
+  // 表示鼠标按下 button 到松开鼠标之间的时间段更改的样式
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+  &.checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
 }
 </style>
