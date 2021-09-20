646347e71ca5f1ff8316168910512c80c253f510
Davis <326461396@qq.com>
2021-09-20 14:46:30 +0800

    完成整体布局

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav />
+  <div class="layout">
+    <Topnav class="nav" />
     <div class="content">
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
@@ -39,23 +39,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-aside {
-  background: lightblue;
-  width: 150px;
-  padding: 16px;
-  > h2 {
-    margin-bottom: 4px;
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
   }
-  > ol {
-    > li {
-      padding: 4px 0;
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    display: flex;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
-  }
-  @media (max-width: 500px) {
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
+    > aside {
+      background: lightblue;
+      width: 150px;
+      padding: 16px;
+      position: fixed;
+      top: 0;
+      left: 0;
+      padding-top: 70px;
+      height: 100%;
+      flex-shrink: 0;
+      > h2 {
+        margin-bottom: 4px;
+      }
+      > ol {
+        > li {
+          padding: 4px 0;
+        }
+      }
+    }
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
+      overflow: auto;
+    }
   }
 }
 </style>
