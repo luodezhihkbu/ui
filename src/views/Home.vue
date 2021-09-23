2b511cff855fd8d968cbd4c58b434d383a573e06
Davis <326461396@qq.com>
2021-09-23 21:45:11 +0800

    引入 icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,6 +10,17 @@
       </p>
     </div>
   </div>
+  <div class="features">
+    <svg class="icon">
+      <use xlink:href="#icon-vue"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-ts"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="#icon-light"></use>
+    </svg>
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,6 +31,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
 .topnavAndBanner {
   background: linear-gradient(
     145deg,
@@ -27,6 +41,7 @@ export default {
     rgba(183, 233, 230, 1) 100%
   );
   .banner {
+    color: $color;
     padding: 100px 0;
     display: flex;
     justify-content: center;
@@ -35,16 +50,23 @@ export default {
     > .actions {
       padding: 8px 0;
       a {
+        background: $green;
+        color: white;
         margin: 0 8px;
-        background: #fff;
         display: inline-block;
-        $h: 28px;
-        height: $h;
-        line-height: $h;
-        border-radius: $h/2;
-        padding: 0 8px;
+        padding: 8px 24px;
+        border-radius: $border-radius;
+        &:hover {
+          text-decoration: none;
+        }
       }
     }
+  }
+}
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
   }
 }
 </style>
