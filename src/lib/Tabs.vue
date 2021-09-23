3c9cb5344be241839f817b71bf9880c059709a19
Davis <326461396@qq.com>
2021-09-23 20:09:11 +0800

    实现 Tab 下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav" ref="container">
+      <!-- 在 v-for 中，由于 ref 中使用了函数，所以需要动态绑定 -->
       <div
         class="gulu-tabs-nav-item"
         v-for="(t, index) in titles"
         :key="index"
         @click="select(t)"
         :class="{ selected: t === selected }"
+        :ref="
+          (el) => {
+            if (t === selected) selectedItem = el;
+          }
+        "
       >
         {{ t }}
       </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <!-- 用 <component> 来渲染 Tab 组件，相当于 <slot>；
@@ -20,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
+import { computed, ref, watchEffect } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -29,6 +36,10 @@ export default {
     },
   },
   setup(props, context) {
+    // 获取模板中 <div> 元素的引用；变量名和 <div> 中 ref 属性的值对应
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
     // 获取 TabDemo.vue 中 Tabs 组件里的子组件 Tab（相当于 Tabs 组件里 <slot> 的内容），返回值是数组类型
     const defaults = context.slots.default();
     // 判断 Tabs 组件里的子组件 Tab 是否是 <Tab> 标签，用 type 属性来获取标签的类型
@@ -48,11 +59,31 @@ export default {
     const select = (title: string) => {
       context.emit("update:selected", title);
     };
+    // watchEffect() 相当于 onMounted() 加 onUpdated()
+    watchEffect(
+      () => {
+        // 让名为 indicator 的 <div> 的宽度和 selectedItem 的 <div> 的宽度相等
+        const { width } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        // 指定名为 indicator 的 <div> 的 left （left 是相对视口的位置）
+        // { left: left1 } 表示析构赋值的重命名
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      },
+      {
+        flush: "post", // 让 watchEffect() 在 DOM 挂载或更新后执行
+      }
+    );
     return {
       defaults,
       titles,
       currentTab,
       select,
+      selectedItem,
+      indicator,
+      container,
     };
   },
 };
@@ -67,6 +98,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -77,6 +109,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      transition: all 250ms;
     }
   }
   &-content {
