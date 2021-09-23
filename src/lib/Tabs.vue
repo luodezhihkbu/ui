0783f9867977b9d66e6609ab01ba7fa68da8bd9c
Davis <326461396@qq.com>
2021-09-23 11:22:27 +0800

    通过嵌套插槽来渲染 Tabs 组件

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>Tabs 组件</div>
+  <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+  <!-- 用 component 来渲染 Tab 组件，相当于 <slot> -->
+  <component v-for="(c, index) in defaults" :is="c" :key="index" /> 
 </template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    // 获取 TabDemo.vue 中 Tabs 组件里的子组件 Tab（相当于 Tabs 组件里 <slot> 的内容），返回值是数组类型
+    const defaults = context.slots.default();
+    console.log(defaults);
+    // 判断 Tabs 组件里的子组件 Tab 是否是 <Tab> 标签，用 type 属性来获取标签的类型
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 的子标签必须是 Tab");
+      }
+    });
+    // 获取 Tabs 组件里的子组件 Tab 中的 title 属性的值
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return {
+      defaults,
+      titles,
+    };
+  },
+};
+</script>
