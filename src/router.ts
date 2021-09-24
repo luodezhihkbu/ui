c1038418590e49bc9d4bfac2e7e6e2c6f0251437
Davis <326461396@qq.com>
2021-09-24 19:25:24 +0800

    增加3个文档组件

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,37 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
+import Intro from "./views/Intro.vue";
+import Install from "./views/Install.vue";
+import GetStarted from "./views/GetStarted.vue";
 
-const history = createWebHashHistory()
+const history = createWebHashHistory();
 export const router = createRouter({
-    history: history,
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/doc',
-            component: Doc,
-            // 嵌套路由
-            children: [
-                { path: '', component: DocDemo },
-                { path: 'switch', component: SwitchDemo },
-                { path: 'button', component: ButtonDemo },
-                { path: 'dialog', component: DialogDemo },
-                { path: 'tabs', component: TabsDemo }
-
-            ]
-        }
-    ]
-})
+  history: history,
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/doc",
+      component: Doc,
+      // 嵌套路由
+      children: [
+        { path: "", component: DocDemo },
+        { path: "intro", component: Intro },
+        { path: "install", component: Install },
+        { path: "get-started", component: GetStarted },
+        { path: "switch", component: SwitchDemo },
+        { path: "button", component: ButtonDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "tabs", component: TabsDemo },
+      ],
+    },
+  ],
+});
