4129b73bc9c9a025e579134c80f1f074445871aa
Davis <326461396@qq.com>
2021-09-26 10:16:07 +0800

    改造 ButtonDemo.vue

---
+++ src/components/Button1.demo.vue
@@ -0,0 +1,20 @@
+<demo>
+常规使用
+</demo>
+
+<template>
+  <div>
+    <Button>按钮</Button>
+    <Button theme="link">按钮</Button>
+    <Button theme="text">按钮</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
---
+++ src/components/Button4.demo.vue
@@ -0,0 +1,19 @@
+<demo>
+支持 disabled
+</demo>
+
+<template>
+  <!-- disabled 是 <button> 自身的属性，默认值为 true ，下面的 disabled 也可以写成 :disabled="true" -->
+  <Button disabled>禁用按钮</Button>
+  <Button theme="link" disabled>禁用链接按钮</Button>
+  <Button theme="text" disabled>禁用按钮</Button>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
