6b0053c78c3b7573dc01ff1e9e42358ff92513fa
Davis <326461396@qq.com>
2021-09-26 19:36:14 +0800

    修复 Bug ：build 之后不加载 md 文件

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -6,20 +6,10 @@
 import { ref } from "vue";
 export default {
   props: {
-    path: {
+    content: {
       type: String,
       required: true,
     },
-  },
-  setup(props) {
-    const content = ref<string>(null);
-    // 使用异步的 import()，将异步的返回值 resul（用 default来获取）赋值给 content.value
-    import(props.path).then((result) => {
-      content.value = result.default;
-    });
-    return {
-      content,
-    };
   },
 };
 </script>
--- src/router.ts
+++ src/router.ts
@@ -5,14 +5,17 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import { h } from 'vue';
-import Markdown from './components/Markdown.vue';
+import { h } from "vue";
+import Markdown from "./components/Markdown.vue";
+import intro from "./markdown/intro.md"; // 导入的变量为字符串类型
+import install from "./markdown/install.md";
+import getStarted from "./markdown/get-started.md";
 
 const history = createWebHashHistory();
-/* h() 会创建一个 <Markdown :path="`../markdown/${filename}.md`" /> 组件；
-并将 path 的值传入到 Markdown.vue 的 Props 里；
+/* h() 会创建一个 <Markdown :content=string /> 组件；
+并将 content 的值传入到 Markdown.vue 的 Props 里；
 必须要绑定 key 值，否则切换路由时组件不会更新 */
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+const md = (string) => h(Markdown, { content: string, key: string });
 export const router = createRouter({
   history: history,
   routes: [
@@ -25,10 +28,10 @@ export const router = createRouter({
       component: Doc,
       // 嵌套路由
       children: [
-        { path: "", redirect: '/doc/intro' },
-        { path: "intro", component: md('intro') },
-        { path: "install", component: md('install') },
-        { path: "get-started", component: md('get-started') },
+        { path: "", redirect: "/doc/intro" },
+        { path: "intro", component: md(intro) },
+        { path: "install", component: md(install) },
+        { path: "get-started", component: md(getStarted) },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
